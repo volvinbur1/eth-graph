3f3ba66ff45fb1b5c6d935e56ed28b622125f374
Volodymyr Burdeinyi <volodymyr.burdeinyi@dxc.com>
2023-01-11 00:52:02 +0200

    + added short path hops call

--- src/main/jobs/spp/ShortestPathJob.scala
+++ src/main/jobs/spp/ShortestPathJob.scala
@@ -9,7 +9,9 @@ object ShortestPathJob {
         val sc = new SparkContext("local[*]", "GraphX")
 
         val graph = new EthereumGraph(sc)
-        graph.dijkstra(0)
+        val res = graph.shortestPath(args(0), args(1))
+        println("Short path value: " + res._1)
+        println("Short path hops: " + res._2)
         sc.stop()
     }
 }
