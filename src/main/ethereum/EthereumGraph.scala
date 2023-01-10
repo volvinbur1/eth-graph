34abc8084291354768686d6ae7c8ae9f977db976
Volodymyr Burdeinyi <volodymyr.burdeinyi@dxc.com>
2023-01-11 00:28:58 +0200

    * save path for the dijkstra algorithm

--- src/main/ethereum/EthereumGraph.scala
+++ src/main/ethereum/EthereumGraph.scala
@@ -12,27 +12,33 @@ class EthereumGraph(sc: SparkContext, walletsFile: String, transactionsFile: Str
     val graph = Graph(vertexs, edges)
 
     def dijkstra(srcId: VertexId) = {
-        var distancesGraph = graph.mapVertices((vId,_) => 
-            (false, if (vId == sourceId) 0.0 else Double.PositiveInfinity))
+        var distancesGraph = graph.mapVertices((vId,_) => (
+                false, 
+                if (vId == sourceId) 0.0 else Double.PositiveInfinity), 
+                if (vId == sourceId) vId :: Nil else Nil
+            )
 
         for (i <- 1L to graph.vertices.count-1) {
-            val zeroValue = (0L,(false,Double.MaxValue))
+            val zeroValue = (0L, (false, Double.MaxValue, Nil))
             val currentVertexId = distancesGraph.vertices
                     .filter((vId, vd) => !vd._1)
                     .fold(zeroValue)((a,b) => math.min(a._2._2, b._2._2))
                     ._1
 
             val newDistances = distancesGraph
-                .aggregateMessages[Double](
-                    ctx => if (ctx.srcId == currentVertexId) ctx.sendToDst(ctx.srcAttr._2 + ctx.attr._1),
-                    (a,b) => math.min(a,b))
+                .aggregateMessages[(Double, List[VertexId])] (
+                    ctx => if (ctx.srcId == currentVertexId) 
+                                ctx.sendToDst((ctx.srcAttr._2 + ctx.attr._1, ctx.srcAttr._3 :+ ctx.dstId)),
+                    (a,b) => math.min(a._1, b._1))
 
             distancesGraph = distancesGraph
-                .outerJoinVertices(newDistances)((vId, vd, newSum) =>
-                    (vd._1 || vId == currentVertexId, math.min(vd._2, newSum.getOrElse(Double.MaxValue))))
+                .outerJoinVertices(newDistances) ((vId, vd, newDist) => (
+                    vd._1 || vId == currentVertexId, 
+                    math.min(vd._2, newDist.getOrElse((Double.MaxValue, Nil))._1),
+                    if (vd._2 < newDist.getOrElse((Double.MaxValue, Nil))._1) vd._2 else newDist.getOrElse((Double.MaxValue, Nil))._2
+                ))
         }
 
-        graph.outerJoinVertices(distancesGraph.vertices)((_, vd, dist) =>
-            (vd, dist.getOrElse((false,Double.MaxValue))._2))
+        distancesGraph
     }
 }
