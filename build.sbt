07c4589acc7ce8312e6bf0a2363804df6b00c9c0
Volodymyr Burdeinyi <volodymyr.burdeinyi@dxc.com>
2023-01-11 13:59:05 +0200

    * fix code run

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "eth-graph"
 
-version := "1.1"
+version := "0.1"
 
 scalaVersion := "2.11.8"
 
@@ -8,6 +8,3 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.2.1",
   "org.apache.spark" %% "spark-graphx" % "2.2.1"
 )
-
-mainClass in (Compile,run) := Some("com.github.ethgraph.jobs.ssp.ShortestPathJob")
-mainClass in (Compile, packageBin) := Some("com.github.ethgraph.jobs.ssp.ShortestPathJob")
--- src/main/scala/com/github/ethgraph/jobs/spp/ShortestPathJob.scala
+++ src/main/scala/com/github/ethgraph/jobs/spp/ShortestPathJob.scala
@@ -4,19 +4,16 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import com.github.ethgraph.ethereum.EthereumGraph
 
-object ShortestPathJob {
-    
-    def main(args: Array[String]) {
-        Logger.getLogger("org").setLevel(Level.ERROR)
-        val sc = new SparkContext("local[*]", "GraphX")
+object ShortestPathJob extends App {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "GraphX")
 
-        val walletsFile = "wallets.tsv"
-        val transactionsFile = "transactions.tsv"
+    val walletsFile = "wallets.tsv"
+    val transactionsFile = "transactions.tsv"
 
-        val graph = new EthereumGraph(sc, walletsFile, transactionsFile)
-        val res = graph.shortestPath(args(0), args(1))
-        println("Short path value: " + res._1)
-        println("Short path hops: " + res._2)
-        sc.stop()
-    }
+    val graph = new EthereumGraph(sc, walletsFile, transactionsFile)
+    val res = graph.shortestPath(args(0), args(1))
+    println("Short path value: " + res._1)
+    println("Short path hops: " + res._2)
+    sc.stop()
 }
