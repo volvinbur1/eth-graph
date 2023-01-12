3bfd9429e5c33262e47a832d0138418e61c1d8be
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2023-01-12 21:43:30 +0200

    * handle exception for input dataset

--- src/main/scala/etherem/InputParser.scala
+++ src/main/scala/etherem/InputParser.scala
@@ -3,13 +3,19 @@ package etherem
 
 import org.apache.spark.graphx.{Edge, VertexId}
 
+import java.io.IOException
 import scala.collection.mutable.ListBuffer
 
 object InputParser {
 
   def getWallets(line: String): Option[(VertexId, String)] = {
     val fields = line.split('\t')
-    Some(fields(0).toLong, fields(1))
+    try {
+      Some(fields(0).toLong, fields(1))
+    } catch {
+      case _: Throwable => println("ERROR: WRONG INPUT for wallets -> " + line)
+      Some(Long.MaxValue, "NotKnownValue")
+    }
   }
 
   def getTransaction(line: String): Option[Edge[(Double, Double, String)]] = {
