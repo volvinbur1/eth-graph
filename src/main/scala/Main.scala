5d1bc8e714f27d550a8e4c5e290504e0eaa37ba4
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2023-01-12 21:46:40 +0200

    * uncommented all algorithm

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,25 +16,25 @@ object Main {
     val transactionsFile = "dataset/test/transactions.tsv"
 
     val graph = new EthereumGraph(sc, walletsFile, transactionsFile)
-//    val shortestPath = graph.shortestPath("A", "F")
-//    println("Shortest path value: " + shortestPath._1.toString)
-//    println("Shortest path hops: " + shortestPath._2.toString)
-//
-//    val tolerance = 0.001
-//    val topRankDynamic = graph.getTopDynamicRank(tolerance, 10, ascending = false)
-//    println("Top rank dynamic: ")
-//    topRankDynamic.foreach(println)
-//
-//    val staticIterCnt = 15
-//    val topTankStatic = graph.getTopStaticRank(staticIterCnt, 10, ascending = false)
-//    println("Top rank static: ")
-//    topTankStatic.foreach(println)
-//
-//    val triangleCounts = graph.countTriangles()
-//    println("Triangles count for 5 subsequent subgraph: " + triangleCounts.toString)
-//
-//    val connectives = graph.connectives()
-//    println("Connectives: " + connectives.toString())
+    val shortestPath = graph.shortestPath("A", "F")
+    println("Shortest path value: " + shortestPath._1.toString)
+    println("Shortest path hops: " + shortestPath._2.toString)
+
+    val tolerance = 0.001
+    val topRankDynamic = graph.getTopDynamicRank(tolerance, 10, ascending = false)
+    println("Top rank dynamic: ")
+    topRankDynamic.foreach(println)
+
+    val staticIterCnt = 15
+    val topTankStatic = graph.getTopStaticRank(staticIterCnt, 10, ascending = false)
+    println("Top rank static: ")
+    topTankStatic.foreach(println)
+
+    val triangleCounts = graph.countTriangles()
+    println("Triangles count for 5 subsequent subgraph: " + triangleCounts.toString)
+
+    val connectives = graph.connectives()
+    println("Connectives: " + connectives.toString())
 
     val propagationIterCnt = 7
     val labelPropagationResult = graph.labelPropagation(propagationIterCnt)
