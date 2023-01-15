56fb00df94bf97f7562904a903af1d60decdf293
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2023-01-15 13:16:45 +0200

    * use cli args instead of hardcode values

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local[*]", "EthGraph")
+    println("Command line params: " + args.mkString("Array(", ", ", ")"))
 
     var sparkConf = new SparkConf()
     sparkConf = sparkConf.set("spark.driver.memory", getSparkDriverMemory(args))
@@ -15,21 +15,22 @@ object Main {
 
     val sc = new SparkContext("local[*]", "EthGraph", conf = sparkConf)
 
-    var srcWallet = "A"
-    var dstWallet = "F"
-    if (args.length > 3) srcWallet = args(3)
-    if (args.length > 4) dstWallet = args(4)
+    // "file:///home/burdeinyi_v_o_gmail_com/wallets.tsv"
+    // "file:///home/burdeinyi_v_o_gmail_com/transactions.tsv"
+    val graph = new EthereumGraph(sc, getWalletsFile(args), getTransactionsFile(args))
 
-    val shortestPath = graph.shortestPath(srcWallet, dstWallet)
-    println("Shortest path value: " + shortestPath._1.toString)
-    println("Shortest path hops: " + shortestPath._2.toString)
+    val srcWallet = getSrcWallet(args)
+    val dstWallet = getDstWallet(args)
+//    val shortestPath = graph.shortestPath(srcWallet, dstWallet)
+//    println("Shortest path value: " + shortestPath._1.toString)
+//    println("Shortest path hops: " + shortestPath._2.toString)
 
-    val tolerance = 0.001
+    val tolerance = getPageRankTolerance(args)
     val topRankDynamic = graph.getTopDynamicRank(tolerance, 10, ascending = false)
     println("Top rank dynamic: ")
     topRankDynamic.foreach(println)
 
-    val staticIterCnt = 15
+    val staticIterCnt = getPageRankIterations(args)
     val topTankStatic = graph.getTopStaticRank(staticIterCnt, 10, ascending = false)
     println("Top rank static: ")
     topTankStatic.foreach(println)
@@ -40,15 +41,15 @@ object Main {
     val connectives = graph.connectives()
     println("Connectives: " + connectives.toString())
 
-    val propagationIterCnt = 7
+    val propagationIterCnt = getLabelPropagationIterations(args)
     val labelPropagationResult = graph.labelPropagation(propagationIterCnt)
     println("Storing labels propagation to file `propagations.tsv`")
-    val propagationWriter = new BufferedWriter(new FileWriter("propagations.tsv"))
+    val propagationWriter = new BufferedWriter(new FileWriter(getLabelPropagationOutputFileIterations(args)))
     labelPropagationResult.foreach(propagationWriter.write)
     propagationWriter.close()
 
-    val diameter = graph.calculateGraphDiameter()
-    println(s"Diameter: ${diameter._1}. From ${diameter._2} to ${diameter._3}")
+//    val diameter = graph.calculateGraphDiameter()
+//    println(s"Diameter: ${diameter._1}. From ${diameter._2} to ${diameter._3}")
 
     val mostSpentVertex = graph.mostSpentVertex()
     println(s"Most spent vertex is ${mostSpentVertex._1} with total value ${mostSpentVertex._2}")
@@ -71,5 +72,66 @@ object Main {
     println(s"Max vertex ${maxVertexOutDegree._1} outDegree value ${maxVertexOutDegree._2}")
 
     sc.stop()
+  }
+
+  private def getWalletsFile(args: Array[String]): String = {
+    val value = findArgument("--wallets", args)
+    if (value.isEmpty) "wallets.tsv" else value
+  }
+
+  private def getTransactionsFile(args: Array[String]): String = {
+    val value = findArgument("--transactions", args)
+    if (value.isEmpty) "transactions.tsv" else value
+  }
+
+  private def getSparkDriverMemory(args: Array[String]): String = {
+    val value = findArgument("--driver-mem", args)
+    if (value.isEmpty) "25g" else value
+  }
+
+  private def getSparkDriverMemoryOverheadFactor(args: Array[String]): String = {
+    val value = findArgument("--mem-overhead-fact", args)
+    if (value.isEmpty) "0.6" else value
+  }
+
+  private def getSrcWallet(args: Array[String]): String = {
+    val value = findArgument("--src-wallet", args)
+    if (value.isEmpty) "" else value
+  }
+
+  private def getDstWallet(args: Array[String]): String = {
+    val value = findArgument("--dst-wallet", args)
+    if (value.isEmpty) "" else value
+  }
+
+  private def getPageRankTolerance(args: Array[String]): Double = {
+    val value = findArgument("--pr-tolerance", args)
+    if (value.isEmpty) 0.001 else value.toDouble
+  }
+
+  private def getPageRankIterations(args: Array[String]): Int = {
+    val value = findArgument("--pr-iterations", args)
+    if (value.isEmpty) 15 else value.toInt
+  }
+
+  private def getLabelPropagationIterations(args: Array[String]): Int = {
+    val value = findArgument("--lp-iterations", args)
+    if (value.isEmpty) 7 else value.toInt
+  }
+
+  private def getLabelPropagationOutputFileIterations(args: Array[String]): String = {
+    val value = findArgument("--lp-output-file", args)
+    if (value.isEmpty) "labelPropagation.tsv" else value
+  }
+
+  private def findArgument(flag: String, args: Array[String]): String = {
+    var returnValue = ""
+    args.foreach(item => {
+        if (item.startsWith(flag)) {
+          val words = item.split("=")
+          if (words.length == 2) returnValue = words(1)
+        }
+      })
+    returnValue
   }
 }
